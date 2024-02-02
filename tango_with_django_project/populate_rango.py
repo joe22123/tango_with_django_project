a50931a6a38dae445af30064cadee312563ee586
Joe Hughes <2783677H@student.gla.ac.uk>
2024-02-02 14:40:00 +0000

    chapter 6 done

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -14,27 +14,35 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',
+         'views':100},
         {'title': 'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views':70},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net'} 
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views':90} 
         ]
     
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'http://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'http://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views':47},
         {'title':'Django Rocks',
-          'url':'http://www.djangorocks.com/'},
+          'url':'http://www.djangorocks.com/',
+          'views':35},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'}
+         'url':'http://www.tangowithdjango.com/',
+         'views':85}
     ]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views':15},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'}
+         'url':'http://flask.pocoo.org',
+         'views':80}
     ]
 
     cats = {'Python': {'pages': python_pages},
@@ -54,7 +62,7 @@ def populate():
         likes=likes/2
         views=views/2
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], views=p['views'])
 
     # Print out the categories we have added
     for c in Category.objects.all():
